69a626b0311d3d7c3262632d3c36745ee4805705
tiya adugna <tiyakefalee@gmail.com>
2020-04-03 17:58:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,17 @@
 # Write your methods here
-def loop_message_five_times
-  
+def loop_message_five_times(message)
+ 
+  while
 end
 
-def loop_message_n_
+def loop_message_n_times
+  
+end 
+
+def output_array
+  
+end 
+
+def return_string_array 
+  
+end 
